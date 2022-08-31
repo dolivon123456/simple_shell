a643b3cb1b39f5f08de1e1cc848295769580965d
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-08-31 21:53:03 +0100

    Create 0-shell_main.c

---
+++ 0-shell_main.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+* shell process 
+*  1. prompt <------
+*  2. input        |
+*  3. parse        |
+*  4. execute ------
+*/
+
+int main (int argc __attribute__((unused)), char **argv)
+{
+ while (1)
+ {
+   printf(" (&) ");
+ }
+}
+  
